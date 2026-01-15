0e69b89caf5243af268f0cb2dd27a47c5d723a83
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-01-14 23:27:24 -0500

    Fix comments

--- etc/skel/.profile.d/30-term.sh
+++ etc/skel/.profile.d/30-term.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# output_scale=1
-# output_scale_eDP_1=2
+# export output_scale=1
+# export output_scale_eDP_1=2
 
 if [ -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY" ]; then
     case $(tty) in /dev/tty[3-6]) exec term tmux;; esac
