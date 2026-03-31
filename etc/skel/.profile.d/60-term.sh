23db4f2d5713eb47bdc8a707ec0bfbee05ca4ff6
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-03-31 17:11:16 -0400

    60-term.sh: fix

--- etc/skel/.profile.d/60-term.sh
+++ etc/skel/.profile.d/60-term.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# export output_scale=1
-# export output_scale_eDP_1=2
-
 if [ -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY" ]; then
-    case $(tty) in /dev/tty[1-9]) exec term -e tmux;; esac
+    case $(tty) in /dev/tty[1-9]) exec tmux;; esac
 fi
