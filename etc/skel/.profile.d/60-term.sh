9a55a07ddc156e93bce6eb81fa1cb1836dc3f0a4
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-04-01 11:48:41 -0400

    Revert "60-term.sh: fix"
    This reverts commit 23db4f2d5713eb47bdc8a707ec0bfbee05ca4ff6.

--- etc/skel/.profile.d/60-term.sh
+++ etc/skel/.profile.d/60-term.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# export output_scale=1
+# export output_scale_eDP_1=2
+
 if [ -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY" ]; then
-    case $(tty) in /dev/tty[1-4]) exec tmux;; esac
+    case $(tty) in /dev/tty[1-4]) exec term -e tmux;; esac
 fi
