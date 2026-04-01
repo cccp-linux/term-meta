8b168b2e69138f54d0e4df49b5fadccf36a1d883
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-04-01 12:15:14 -0400

    60-term.sh: tty doesn't work inside emulators

--- etc/skel/.profile.d/60-term.sh
+++ etc/skel/.profile.d/60-term.sh
@@ -4,5 +4,5 @@
 # export output_scale_eDP_1=2
 
 if [ -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY" ]; then
-    case $(tty) in /dev/tty[1-4]) exec term -e tmux;; esac
+    case "$XDG_VTNR" in [1-4]) exec term -e tmux;; esac
 fi
