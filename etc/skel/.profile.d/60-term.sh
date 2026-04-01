d864248cf1687338936e6e159711f09b95cb3b8e
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-04-01 12:15:54 -0400

    60-term.sh: don't do Inception

--- etc/skel/.profile.d/60-term.sh
+++ etc/skel/.profile.d/60-term.sh
@@ -3,6 +3,6 @@
 # export output_scale=1
 # export output_scale_eDP_1=2
 
-if [ -z "$DISPLAY" -a -z "$WAYLAND_DISPLAY" ]; then
+if [ -z "$DISPLAY" -a -z "$TMUX" -a -z "$WAYLAND_DISPLAY" ]; then
     case "$XDG_VTNR" in [1-4]) exec term -e tmux;; esac
 fi
